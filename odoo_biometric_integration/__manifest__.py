9c6bfdc1eaa32fa27c23a1879a4f4e159387879c
dimasaryonovantri04-create <dimasaryonovantri04@gmail.com>
2026-02-17 20:33:35 +0700

    Update __manifest__.py

--- odoo_biometric_integration/__manifest__.py
+++ odoo_biometric_integration/__manifest__.py
@@ -1,28 +1,34 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'Odoo Pro Biometric Integration',
-    'version': '8.0.1.0.1', 
+    'version': '8.0.1.0.1',
     'summary': 'Professional biometric integration with advanced logging, UI, and configuration.',
     'author': 'Dimas Aryo Novantri',
     'category': 'Human Resources/Attendances',
-    
+
     # --- BAGIAN UNTUK JUALAN ---
-    'license': 'OPL-1',  
+    'license': 'OPL-1',
     'price': 350.00,
     'currency': 'USD',
-    # -----------------------------
+
+    'images': ['static/description/banner.png'],
+
+
+    'long_description': open('static/description/index.html').read(),
     
+    # === PENAMBAHAN PENTING SELESAI ===
+
     'depends': [
         'hr_attendance',
     ],
     'data': [
         'security/ir.model.access.csv',
         'views/res_config_settings_views.xml',
-        'views/hr_biometric_data_views.xml', 
-        'views/hr_attendance_views.xml',   
+        'views/hr_biometric_data_views.xml',
+        'views/hr_attendance_views.xml',
         'views/biometric_menus.xml',
     ],
     'installable': True,
     'application': True,
     'auto_install': False,
 }
-
